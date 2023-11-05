a8682f886714c6d069da38db5c0be1cddb537fbf
Terilks <149944437+Terilks@users.noreply.github.com>
2023-11-05 16:06:58 +0300

    Update game.c
    Код тот же но распределил его по функциям

--- game.c
+++ game.c
@@ -12,21 +12,14 @@ int main() {
     int canvas[HEIGHT][WIDTH];
     LaunchGame(canvas_before, canvas);
 
-
-
     return 0;
 }
 
-
-void InitSettings() {
-    srand(time(NULL));
-}
+void InitSettings() { srand(time(NULL)); }
 
 void RandomCells(int canvas[HEIGHT][WIDTH]) {
     for (int i = 0; i < HEIGHT; i++)
-        for (int j = 0; j < WIDTH; j++)
-            canvas[i][j] = rand() % 2;
-
+        for (int j = 0; j < WIDTH; j++) canvas[i][j] = rand() % 2;
 
     // for (int y = 0; y < HEIGHT; y++) {
     //         for (int x = 0; x < WIDTH; x++) {
@@ -47,47 +40,56 @@ int IsHaveNeighbor(int canvas[HEIGHT][WIDTH], int y, int x) {
     }
 
     if (canvas[y][x] == 1) {
-        if (count_lives == 2 || count_lives == 3) flag = 1;
-        else flag = 0;
-    }
-    else {
-        if (count_lives == 3) flag = 1;
-        else flag = 0;
+        if (count_lives == 2 || count_lives == 3)
+            flag = 1;
+        else
+            flag = 0;
+    } else {
+        if (count_lives == 3)
+            flag = 1;
+        else
+            flag = 0;
     }
 
     return flag;
 }
 
-void LaunchGame(int canvas_before[HEIGHT][WIDTH], int canvas[HEIGHT][WIDTH]) {
-    InitSettings();
-    
-    RandomCells(canvas_before);
-
-
-    while(1) {
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                if (canvas_before[y][x] == 1)
-                    printf("#");
-                else 
-                    printf(" ");
-            }
+void DrawGame(int canvas_before[HEIGHT][WIDTH], int canvas[HEIGHT][WIDTH]) {
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            if (canvas_before[y][x] == 1)
+                printf("#");
+            else
+                printf(" ");
+        }
 
         printf("\n");
-        }
+    }
 
-        char c = getchar();
-        printf("\e[1;1H\e[2J");
+    char c = getchar();
+    printf("\e[1;1H\e[2J");
+}
 
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                canvas[y][x] = canvas_before[y][x];
-            }
+void CreateNextStep(int canvas_before[HEIGHT][WIDTH], int canvas[HEIGHT][WIDTH]) {
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            canvas[y][x] = canvas_before[y][x];
         }
-                for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                canvas_before[y][x] = IsHaveNeighbor(canvas, y, x);
-            }
+    }
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            canvas_before[y][x] = IsHaveNeighbor(canvas, y, x);
         }
+    }
+}
+
+void LaunchGame(int canvas_before[HEIGHT][WIDTH], int canvas[HEIGHT][WIDTH]) {
+    InitSettings();
+
+    RandomCells(canvas_before);
+
+    while (1) {
+        DrawGame(canvas_before, canvas);
+        CreateNextStep(canvas_before, canvas);
     }
 }
