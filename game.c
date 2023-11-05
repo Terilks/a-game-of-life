ed675c478916b61d0f998a4feaec158f98ae219b
Terilks <149944437+Terilks@users.noreply.github.com>
2023-11-05 21:24:29 +0300

    Рабочий код

--- game.c
+++ game.c
@@ -8,33 +8,21 @@
 #define HEIGHT 25
 
 void LaunchGame(int canvas_before[HEIGHT][WIDTH], int canvas[HEIGHT][WIDTH]);
-void InitSettings(int canvas_before[HEIGHT][WIDTH]) {
-    srand(time(NULL));
-    initscr();
-    curs_set(0);
-    noecho();
-    timeout(0);
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    attron(COLOR_PAIR(1));
-    keypad(stdscr, TRUE);
-
-    for (int i = 0; i < HEIGHT; i++)
-        for (int j = 0; j < WIDTH; j++) canvas_before[i][j] = 0;
-}
-
-void RandomCells(int canvas[HEIGHT][WIDTH]) {
-    for (int i = 0; i < HEIGHT; i++)
-        for (int j = 0; j < WIDTH; j++) canvas[i][j] = rand() % 2;
-}
+void InitSettings(int canvas_before[HEIGHT][WIDTH]);
+void RandomCells(int canvas[HEIGHT][WIDTH]);
 
 int main() {
-    int canvas_before[HEIGHT][WIDTH];
     int canvas[HEIGHT][WIDTH];
+    int canvas_future[HEIGHT][WIDTH];
 
-    InitSettings(canvas_before);
+    InitSettings(canvas);
+    LaunchGame(canvas, canvas_future);
 
-    if (isatty(fileno(stdin))) {
+    return 0;
+}
+
+void ReadCanvas(int canvas_before[HEIGHT][WIDTH]) {
+        if (isatty(fileno(stdin))) {
         RandomCells(canvas_before);
     } else {
         for (int i = 0; i < HEIGHT; i++) {
@@ -47,14 +35,29 @@ int main() {
     if (freopen("/dev/tty", "r", stdin) != NULL) {
         TRUE;
     }
-
-    LaunchGame(canvas_before, canvas);
-
-    return 0;
 }
 
-int IsHaveNeighbor(int canvas[HEIGHT][WIDTH], int y, int x) {
-    int flag = 0;
+void RandomCells(int canvas[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++)
+        for (int j = 0; j < WIDTH; j++) canvas[i][j] = rand() % 2;
+}
+
+void InitSettings(int canvas_before[HEIGHT][WIDTH]) {
+    srand(time(NULL));
+
+    initscr();
+    curs_set(0);
+    noecho();
+    timeout(0);
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    attron(COLOR_PAIR(1));
+    keypad(stdscr, TRUE);
+
+    ReadCanvas(canvas_before);
+}
+
+int GetCountNeighbor(int canvas[HEIGHT][WIDTH], int y, int x) {
     int count_lives = 0;
 
     for (int dy = -1; dy <= 1; dy++) {
@@ -63,6 +66,12 @@ int IsHaveNeighbor(int canvas[HEIGHT][WIDTH], int y, int x) {
             if (canvas[(y + dy + HEIGHT) % HEIGHT][(x + dx + WIDTH) % WIDTH] == 1) count_lives++;
         }
     }
+    return count_lives;
+}
+
+int IsWillLiveCeil(int canvas[HEIGHT][WIDTH], int y, int x) {
+    int flag = 0;
+    int count_lives = GetCountNeighbor(canvas, y, x);
 
     if (canvas[y][x] == 1) {
         if (count_lives == 2 || count_lives == 3)
@@ -87,7 +96,7 @@ void CreateNextStep(int canvas_before[HEIGHT][WIDTH], int canvas[HEIGHT][WIDTH])
     }
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            canvas_before[y][x] = IsHaveNeighbor(canvas, y, x);
+            canvas_before[y][x] = IsWillLiveCeil(canvas, y, x);
         }
     }
 }
